1648663e12c56e93f8b9e26a736493369b2437ba
EMurrell <82235988+EMurrell@users.noreply.github.com>
2021-11-22 09:31:55 -0500

    fix twitter card

--- components/MetaData.js
+++ components/MetaData.js
@@ -13,7 +13,7 @@ export default function Meta() {
         property="og:description"
         content="Eric Murrell- Full Stack Web Developer"
       />
-      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@MurrellWeb" />
       <meta name="twitter:image" content="/metaBlue3.jpg" />
       <meta name="twitter:title" content="EMurrell.com" />
